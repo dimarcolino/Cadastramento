fe8ea261632e4444913c43326e359321afd486ac
dimarcolino <142554665+dimarcolino@users.noreply.github.com>
2025-05-13 18:22:05 -0300

    Add files via upload

---
+++ crud_git/frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Inicio" subtitle="Segundo Projeto do capitulo">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de um cadastro desenvolvido em
+      react
+    </p>
+  </Main>
+);
